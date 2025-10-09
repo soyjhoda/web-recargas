e93068e066d6e15df90d4a8134ee52917392cd75
Soy Jhoda <pruebasjhoda@gmail.com>
2025-10-08 20:41:53 -0400

    Actualizo supabase.js para usar variables de entorno

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://alkzivgbqbdhvyvswfnr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3ppdmdicWJkaHZ5dnN3Zm5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0NjEyMzAsImV4cCI6MjA3NTAzNzIzMH0.fKYu6vmXEFZA-K-H3h2JTjTK3ZDPqrny1psihtl5I1M";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://alkzivgbqbdhvyvswfnr.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "tu_clave_de_respaldo";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
